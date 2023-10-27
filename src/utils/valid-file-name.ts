7e22d8622c129693a5d3d22f0690cbeb896e378a
Evan Bonsignori <evanabonsignori@gmail.com>
2023-10-27 16:07:49 -0700

    allow special characters in native mode

---
+++ src/utils/valid-file-name.ts
@@ -0,0 +1,9 @@
+const validCharRegex =
+	/[a-z0-9\\$\\-\\_\\!\\%\\"\\'\\.\\,\\*\\&\\(\\)\\;\\{\\}\\+\\=\\~\\`\\?)]/i;
+
+export const isValidFileNameCharacter = (char: string) => {
+	if (char === "\\") {
+		return false;
+	}
+	return validCharRegex.test(char);
+};
